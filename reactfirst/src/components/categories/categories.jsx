1555887da45ea29bf6f03526dd21494e39890960
pedrusouza <pomsouza08@gmail.com>
2024-05-22 15:44:48 -0300

    e-commerce começando aqui

---
+++ reactfirst/src/components/categories/categories.jsx
@@ -0,0 +1,12 @@
+import { Category } from "../category/category"
+
+export function Categories( { categories } )){
+
+    console.log()
+    return {
+        <div>
+        <h1> Teste </h1>
+        </div>
+
+    }
+}
---
+++ reactfirst/src/components/category/category.jsx
@@ -0,0 +1,10 @@
+export function Category ({ category }){
+console.log(category)
+return {
+
+    <div>
+        <h1> Teste Category </h1>
+    </div>
+
+}
+}
